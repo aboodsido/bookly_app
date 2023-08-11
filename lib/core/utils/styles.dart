79977dcc6b16f4bf16210ea7d4cd0376cf5fffd4
Abdullah Abo-Sido <odod41907@gmail.com>
2023-08-11 18:15:56 +0300

    create a book model

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  
   static const titleMeduim =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
   static const titleLarge20 = TextStyle(
