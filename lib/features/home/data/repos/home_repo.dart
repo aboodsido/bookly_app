8a6122336e54a9a11431c8aa60a1f760bef11005
Abdullah Abo-Sido <odod41907@gmail.com>
2023-08-12 14:23:36 +0300

    create a failure class with Either class

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../models/book_model/book_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+}
