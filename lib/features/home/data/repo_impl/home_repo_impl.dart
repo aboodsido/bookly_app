2e55fe7fb4b30105a5cea2d006a325f67c1f066a
Abdullah Abo-Sido <odod41907@gmail.com>
2023-08-13 10:51:42 +0300

    create server failure class to handle errors

--- lib/features/home/data/repo_impl/home_repo_impl.dart
+++ lib/features/home/data/repo_impl/home_repo_impl.dart
@@ -1,14 +1,41 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 import '../../../../core/errors/failure.dart';
 import '../models/book_model/book_model.dart';
 import '../repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      const String endPoint =
+          'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming';
+      var data = await apiService.get(endPoint: endPoint);
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(e.toString()),
+      );
+    }
   }
 
   @override
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -5,5 +5,5 @@ import '../models/book_model/book_model.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
 }
